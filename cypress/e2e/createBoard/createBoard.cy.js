7c47315cb6c279da4d391e6d3dc07cd1fe507c5f
qusaialiu <132788714+qusaialiu@users.noreply.github.com>
2024-12-22 01:56:36 +0300

    update

--- cypress/e2e/createBoard/createBoard.cy.js
+++ cypress/e2e/createBoard/createBoard.cy.js
@@ -15,7 +15,7 @@ When("Choose create board option", () => {
 
 })
 When("Types board name in board title field", () => {
-    cy.findByTestId("header-create-board-button")
+
 })
 When("Clicks on Create button", () => {
 
